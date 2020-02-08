4d96753448f17a1e8be689da861b476c47f9dfb0
setarehmofidi <setareh.seyedmofidi@pcc.edu>
2020-02-07 16:46:29 -0800

    first commit

---
+++ sketch.js
@@ -0,0 +1,156 @@
+let frames=1
+let xrot= -1
+let spin=0
+let phaseSlider;
+let zlider;
+let whichDay
+function setup() {
+  createCanvas(windowWidth,windowHeight,WEBGL);
+   phaseSlider = createSlider(-3,3,0,0,1);
+   phaseSlider.position(width/2, 80);
+   zlider = createSlider(-3,3,0,0,1);
+   zlider.position(width/4, 80);
+   whichDay = createSlider(1,30,15,1);
+   whichDay.position(width/3, height/1.5);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+background(0);
+push()
+translate(-width/2,-height/2)
+ var galaxy = { 
+  locationX : random(width),
+  locationY : random(height),
+  size : random(1,6)
+  }
+  ellipse(galaxy.locationX ,galaxy.locationY, galaxy.size, galaxy.size);
+pop()
+
+console.log(whichDay.value())
+
+  
+drawMoon (whichDay.value(),width*0.1)
+ 
+
+ 
+}
+
+function drawMoon(day,moonsize){
+ 
+  moonPhase = phaseSlider.value();
+  moonZ= zlider.value();
+  //directionalLight(250, 255, 250, -moonPhase,0,moonZ);
+    if (day==1){
+      directionalLight(200,200,200,-2,0,3)
+    }
+    if (day==2){
+    directionalLight(200,200,200,-3,0,3)
+    }
+    if (day==3){
+    directionalLight(200,200,200,-3,0,2)
+    }
+    if (day==4){
+    directionalLight(200,200,200,-3,0,1.5)
+    }
+    if (day==5){
+    directionalLight(200,200,200,-3,0,1)
+    }
+    if (day==6){
+    directionalLight(200,200,200,-3,0,0.5)
+    }
+    if (day==7){
+    directionalLight(200,200,200,-3,0,0)
+    }
+    if (day==8){
+    directionalLight(200,200,200,-3,0,-0.5)
+    }
+    if (day==9){
+    directionalLight(200,200,200,-3,0,-1)
+    }
+    if (day==10){
+    directionalLight(200,200,200,-3,0,-1.5)
+    }
+    if (day==11){
+    directionalLight(200,200,200,-3,0,-2)
+    }
+    if (day==12){
+    directionalLight(200,200,200,-3,0,-2.5)
+    }
+    if (day==13){
+    directionalLight(200,200,200,-3,0,-3)
+    }
+    if (day==14){
+    directionalLight(200,200,200,-2,0,-3)
+    }
+    if (day==15){
+    directionalLight(200,200,200,0,0,-3)
+    }
+    if (day==16){
+    directionalLight(200,200,200,1,0,-3)
+    }
+    if (day==17){
+    directionalLight(200,200,200,2,0,-3)
+    }
+    if (day==18){
+    directionalLight(200,200,200,2.5,0,-3)
+    }
+    if (day==19){
+    directionalLight(200,200,200,3,0,-3)
+    }
+    if (day==20){
+    directionalLight(200,200,200,3,0,-2.5)
+    }
+    if (day==21){
+    directionalLight(200,200,200,3,0,-2)
+    }
+    if (day==22){
+    directionalLight(200,200,200,3,0,-1.5)
+    }
+    if (day==23){
+    directionalLight(200,200,200,3,0,-1)
+    }
+    if (day==24){
+    directionalLight(200,200,200,3,0,-0.5)
+    }
+    if (day==25){
+    directionalLight(200,200,200,3,0,0)
+    }
+    if (day==26){
+    directionalLight(200,200,200,3,0,0.5)
+    }
+    if (day==27){
+    directionalLight(200,200,200,3,0,1)
+    }
+    if (day==28){
+    directionalLight(200,200,200,3,0,2)
+    }
+    if (day==29){
+    directionalLight(200,200,200,3,0,3)
+    }
+    if (day==30){
+    directionalLight(200,200,200,2,0,3)
+    }
+
+
+
+
+
+
+
+   // rotateY(radians(spin%360))
+
+    translate(0,0,0)
+
+//translate(width/40,height/35);
+
+
+noStroke();
+
+sphere(moonsize);
+
+ spin++;
+}
